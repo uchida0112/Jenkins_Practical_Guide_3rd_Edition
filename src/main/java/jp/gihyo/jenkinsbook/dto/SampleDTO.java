fe7a3febd9bd21b3c73035c18f3ae69cac5606e9
uchida0112 <uchida@CentOS-docker.2bvfemwh2u0ehm4edvwgy4hlmb.lx.internal.cloudapp.net>
2020-06-14 09:05:15 +0000

    Respond to warnings of Checkstyle and FindBugs

--- src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
+++ src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
@@ -21,12 +21,12 @@ public class SampleDTO implements Serializable {
 
     /**
      * Store the user name.
-     * @param firstName first name of the user
-     * @param lastName last name of the user
+     * @param fstName first name of the user
+     * @param lstName last name of the user
      */
-    public SampleDTO(final String firstName, final String lastName) {
-        this.setFirstName(firstName);
-        this.setLastName(lastName);
+    public SampleDTO(final String fstName, final String lstName) {
+        this.setFirstName(fstName);
+        this.setLastName(lstName);
         this.setMessage("Hello");
     }
 
@@ -58,16 +58,16 @@ public class SampleDTO implements Serializable {
      * Set first name of the user.
      * @param firstName first name of the user
      */
-    public final void setFirstName(final String firstName) {
-        this.firstName = firstName;
+    public final void setFirstName(final String fstName) {
+        this.firstName = fstName;
     }
 
     /**
      * Set last name of the user.
      * @param lastName last name of the user
      */
-    public final void setLastName(final String lastName) {
-        this.lastName = lastName;
+    public final void setLastName(final String lstName) {
+        this.lastName = lstName;
     }
 
     /**
